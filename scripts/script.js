c2b73e9c71d186e10c353fa7247d60db0ff8c803
Deon <81569904+deonblok@users.noreply.github.com>
2021-09-24 10:12:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var navButton = document.querySelector("nav button");
+
+navButton.addEventListener("click", verschijnMenu);
+
+function verschijnMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("verschijnMenu");
+}
